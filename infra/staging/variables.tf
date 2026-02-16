cf170981ed6d9bff474fbb1c9ef293fe73c1821b
Kacper Fila <kacperfila26@gmail.com>
2026-02-16 19:21:05 +0100

    refactor: update Terraform configuration

--- infra/staging/variables.tf
+++ infra/staging/variables.tf
@@ -3,3 +3,11 @@ variable "image_tag" {
   default = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
   description = "Docker image tag to deploy to Container App"
 }
+
+variable "allowed_ips" {
+  type = list(string)
+  default = [
+    "0.0.0.0"        # Azure services
+  ]
+  description = "Allowed IPs by PostgreSQL Firewall"
+}
