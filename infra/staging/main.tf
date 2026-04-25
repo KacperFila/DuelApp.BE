c224262c8acdbec850861c870cb891a602cc1fa7
Kacper Fila <kacperfila26@gmail.com>
2026-04-25 18:56:40 +0200

    feat: configure keycloak realm and clients

--- infra/staging/main.tf
+++ infra/staging/main.tf
@@ -63,12 +63,12 @@ resource "azurerm_container_app_environment" "duelapp_env" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "duelapp_kv" {
-  name                     = "staging-duelapp-kv"
-  location                 = azurerm_resource_group.rg_duelapp_be_staging.location
-  resource_group_name      = azurerm_resource_group.rg_duelapp_be_staging.name
-  tenant_id                = data.azurerm_client_config.current.tenant_id
-  sku_name                 = "standard"
-  purge_protection_enabled = false
+  name                      = "staging-duelapp-kv"
+  location                  = azurerm_resource_group.rg_duelapp_be_staging.location
+  resource_group_name       = azurerm_resource_group.rg_duelapp_be_staging.name
+  tenant_id                 = data.azurerm_client_config.current.tenant_id
+  sku_name                  = "standard"
+  purge_protection_enabled  = false
   enable_rbac_authorization = true
 
   tags = {
@@ -156,11 +156,11 @@ resource "azurerm_linux_web_app" "keycloak" {
   location            = azurerm_resource_group.rg_duelapp_be_staging.location
   resource_group_name = azurerm_resource_group.rg_duelapp_be_staging.name
 
-  service_plan_id         = azurerm_service_plan.spkeycloak.id
-  https_only              = true
+  service_plan_id = azurerm_service_plan.spkeycloak.id
+  https_only      = true
 
   site_config {
-    always_on = false
+    always_on                               = false
     container_registry_use_managed_identity = true
 
     application_stack {
@@ -175,15 +175,15 @@ resource "azurerm_linux_web_app" "keycloak" {
 
   app_settings = {
     "DOCKER_REGISTRY_SERVER_URL" = "https://${azurerm_container_registry.duelapp_acr.name}.azurecr.io"
-    "KC_DB": "postgres"
-    "KC_DB_URL_HOST": azurerm_postgresql_flexible_server.postgres.fqdn
-    "KC_DB_URL_PORT": 5432
-    "KC_DB_URL_DATABASE": azurerm_postgresql_flexible_server_database.keycloak_db.name
-    "KC_DB_USERNAME": "psqladmin"
+    "KC_DB" : "postgres"
+    "KC_DB_URL_HOST" : azurerm_postgresql_flexible_server.postgres.fqdn
+    "KC_DB_URL_PORT" : 5432
+    "KC_DB_URL_DATABASE" : azurerm_postgresql_flexible_server_database.keycloak_db.name
+    "KC_DB_USERNAME" : "psqladmin"
     "KC_DB_PASSWORD" = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.postgres_admin_password.id})"
-    "KC_PROXY": "edge"
-    "WEBSITES_PORT": 8080
-    "KEYCLOAK_ADMIN" = "admin"
+    "KC_PROXY" : "edge"
+    "WEBSITES_PORT" : 8080
+    "KEYCLOAK_ADMIN"          = "admin"
     "KEYCLOAK_ADMIN_PASSWORD" = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.keycloak_admin_password.id})"
   }
 }
@@ -198,6 +198,84 @@ resource "azurerm_service_plan" "spkeycloak" {
 
   os_type  = "Linux"
   sku_name = "F1"
+}
+
+# =====================================================
+# Keycloak realm
+# =====================================================
+resource "keycloak_realm" "duelapp-realm" {
+  realm        = "duelapp-realm"
+  enabled      = true
+  display_name = "duelapp-realm"
+
+  login_theme = "base"
+
+  access_code_lifespan = "1h"
+
+  ssl_required    = "external"
+  password_policy = "upperCase(1) and length(8) and forceExpiredPasswordChange(365) and notUsername"
+
+  security_defenses {
+    headers {
+      x_frame_options                     = "DENY"
+      content_security_policy             = "frame-src 'self'; frame-ancestors 'self'; object-src 'none';"
+      content_security_policy_report_only = ""
+      x_content_type_options              = "nosniff"
+      x_robots_tag                        = "none"
+      x_xss_protection                    = "1; mode=block"
+      strict_transport_security           = "max-age=31536000; includeSubDomains"
+    }
+    brute_force_detection {
+      permanent_lockout                = false
+      max_login_failures               = 30
+      wait_increment_seconds           = 60
+      quick_login_check_milli_seconds  = 1000
+      minimum_quick_login_wait_seconds = 60
+      max_failure_wait_seconds         = 900
+      failure_reset_time_seconds       = 43200
+    }
+  }
+}
+
+# =====================================================
+# Keycloak clients
+# =====================================================
+resource "keycloak_openid_client" "duelapp_fe_keycloak_client" {
+  realm_id  = keycloak_realm.duelapp-realm.id
+  client_id = "duelapp-fe-keycloak-client"
+
+  name    = "DuelApp FE"
+  enabled = true
+
+  access_type                = "PUBLIC"
+  standard_flow_enabled      = true
+  pkce_code_challenge_method = "S256"
+
+  valid_redirect_uris = [
+    "https://staging-duelapp-fe98179.azurewebsites.net/*"
+  ]
+
+  login_theme = "keycloak"
+}
+
+resource "keycloak_openid_client" "duelapp_be_keycloak_client" {
+  realm_id  = keycloak_realm.duelapp-realm.id
+  client_id = "duelapp-be-keycloak-client"
+
+  name    = "DuelApp BE"
+  enabled = true
+
+  access_type = "CONFIDENTIAL"
+
+  standard_flow_enabled        = true
+  service_accounts_enabled     = true
+  direct_access_grants_enabled = false
+
+  web_origins = [
+    "https://staging-duelapp-fe98179.azurewebsites.net"
+  ]
+
+  login_theme = "keycloak"
 }
 
 # =====================================================
