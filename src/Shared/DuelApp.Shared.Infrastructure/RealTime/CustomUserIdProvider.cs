fc0cd518ad3ff472e98cfa624ff157f401db772d
Kacper Fila <kacperfila26@gmail.com>
2026-04-28 16:28:44 +0200

    fix: add fallback claim used to obtain userId in CustomUserIdProvider

--- src/Shared/DuelApp.Shared.Infrastructure/RealTime/CustomUserIdProvider.cs
+++ src/Shared/DuelApp.Shared.Infrastructure/RealTime/CustomUserIdProvider.cs
@@ -7,6 +7,8 @@ public sealed class CustomUserIdProvider : IUserIdProvider
 {
     public string? GetUserId(HubConnectionContext connection)
     {
-        return connection.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var userId = connection.User.FindFirst(ClaimTypes.NameIdentifier)?.Value ??
+                           connection.User.FindFirst("sub")?.Value;
+        return userId;
     }
 }
