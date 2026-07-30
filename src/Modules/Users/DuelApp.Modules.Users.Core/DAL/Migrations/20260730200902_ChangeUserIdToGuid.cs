4eeb06a3d8da6cc35f2d8cc1e55da34e609f7e58
Kacper Fila <kacperfila26@gmail.com>
2026-07-30 22:11:10 +0200

    feat: add migration for changing userId to guid

---
+++ src/Modules/Users/DuelApp.Modules.Users.Core/DAL/Migrations/20260730200902_ChangeUserIdToGuid.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DuelApp.Modules.Users.Core.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeUserIdToGuid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                schema: "users",
+                table: "Users",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                schema: "users",
+                table: "Users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+    }
+}
