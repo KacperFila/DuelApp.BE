89261e7b9fb5461fc205fdc80283fea67bc3f336
Kacper Fila <kacperfila26@gmail.com>
2026-07-30 23:00:34 +0200

    feat: rename users db context schema name

---
+++ src/Modules/Users/DuelApp.Modules.Users.Core/DAL/Migrations/20260730210509_RenameUsersSchema.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DuelApp.Modules.Users.Core.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameUsersSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                schema: "users",
+                newName: "Users",
+                newSchema: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                schema: "Users",
+                newName: "Users",
+                newSchema: "users");
+        }
+    }
+}
